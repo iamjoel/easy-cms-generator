4d9661fd2aeb0e56e3800915907fb0572732a43d
joel <iamjoel007@gmail.com>
2018-04-19 15:05:50 +0800

    enchance

--- server/config/database.js
+++
@@ -1,8 +0,0 @@
-// mysql 配置
-module.exports = { 
-  connectionLimit : 10,
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'amusement_admin_config'
-}
--- server/config/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  port: 8002,
-  mysql: require('./database'),
-  feCodeRootPath: '/Users/jinweiqiang/front-end/template/admin'
-}
