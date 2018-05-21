6a68e3c32a0f3cc9cfa06b13684d11c8488d981c
joel <iamjoel007@gmail.com>
2018-05-21 17:51:54 +0800

    fin data order

--- server/api/utils/commonCRUD.js
+++ server/api/utils/commonCRUD.js
@@ -2,15 +2,23 @@ const guidFn = require('../../utils/guid')
 const apiFormat = require('../../utils/apiFormat')
 
 module.exports = function (tableName) {
+  var sortKey = 'updateAt'
+  var sortType = 'desc' // 降序
+
+  if(tableName === 'menu') {
+    sortKey = 'order'
+    sortType = 'asc'
+  }
   return {
     list(req, res, pool) {
       try {
-        var results = global.db.get(tableName).value()
+        var results = global.db.get(tableName)
+                            .orderBy(sortKey, sortType)
+                            .value()
         res.send(apiFormat.success(results))
       } catch(error) {
         res.send(apiFormat.error(error))
       }
-      
     },
     detail(req, res, pool) {
       var results = global.db
@@ -26,7 +34,8 @@ module.exports = function (tableName) {
         global.db
             .get(tableName)
             .push(Object.assign({
-              id: guidFn()
+              id: guidFn(),
+              updateAt: Date.now()
             }, req.body))
             .write()
         res.send(apiFormat.success())
@@ -42,6 +51,9 @@ module.exports = function (tableName) {
               id: req.params.id,
             })
             .assign(req.body)
+            .assign({
+              updateAt: Date.now()
+            })
             .write()
         res.send(apiFormat.success())
       } catch(error) {
